b23cecc19266a6272b63da7509932cf1bfdf2c6d
godstark82 <godstark82@gmail.com>
2021-05-17 17:47:42 +0530

    null safety enabled

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 
 ## 0.0.2 
 * Background Color Change Option Enabled
+
+## 0.0.3
+* Enable null safety
--- lib/easy_bottom_bar.dart
+++ lib/easy_bottom_bar.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class EasyBottomBar extends StatefulWidget {
-  List<Widget> tabs;
-  int currentindex = 0;
-  List<BottomNavigationBarItem> bottomItems;
-  Color backgroundColor = Color.fromRGBO(58, 66, 86, 1.0);
+  List<Widget >? tabs;
+  int ?currentindex = 0;
+  List<BottomNavigationBarItem>? bottomItems;
+  Color ?backgroundColor = Color.fromRGBO(58, 66, 86, 1.0);
 
   EasyBottomBar({
-    @required this.tabs,
-    @required this.bottomItems,
+    required this.tabs,
+    required this.bottomItems,
     this.currentindex,
     this.backgroundColor,
   });
@@ -22,14 +22,14 @@ class _EasyBottomBarState extends State<EasyBottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.tabs[widget.currentindex],
+      body: widget.tabs![widget.currentindex!],
       bottomNavigationBar: BottomNavigationBar(
-        items: widget.bottomItems,
+        items: widget.bottomItems!,
         unselectedLabelStyle: TextStyle(color: Colors.white),
         unselectedItemColor: Colors.white,
         showUnselectedLabels: false,
         selectedFontSize: 17,
-        currentIndex: widget.currentindex,
+        currentIndex: widget.currentindex!,
         backgroundColor: widget.backgroundColor,
         selectedItemColor: Colors.greenAccent,
         // Change Tabs ...
